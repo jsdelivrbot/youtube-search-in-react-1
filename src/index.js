a5e6c3f002ec451e6afbf65507c930ae9af2dc84
Taulut Hossain Washi <washinpi@gmail.com>
2017-09-16 11:48:56 +0600

    imple youtube api ,state and props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = "AIzaSyBVUVnb9_zqCW04O5-y3qCXSVi1dbHvY78";
 
-const App = function () {
-    return <div>Hello from react</div>
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            videos:[]
+        }
+
+        YTSearch({key:API_KEY,term:'surfboard'}, (videos) => {
+            this.setState({videos:videos})
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
+
 }
 
 ReactDOM.render(
     <App/>,
     document.getElementById('app')
 );
+
+
